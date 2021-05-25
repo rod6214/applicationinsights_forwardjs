e5c14fcab1df513a5cada7b83cda5fe7ccea1b59
Nelson Amador <rod6214@gmail.com>
2021-05-24 22:25:18 -0300

    Created entry point

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
+const argv = require('minimist')(process.argv.slice(2));
 const ailognginx = require('./ailognginx');
 
-var pattern = '$remote_addr [$time_local] $command "$app" "$name" "$args" - $bytes_received $bytes_sent "$pageurl" "$flashver" ($session_readable_time)'
+const pattern = '$remote_addr [$time_local] $command "$app" "$name" "$args" - $bytes_received $bytes_sent "$pageurl" "$flashver" ($session_readable_time)'
 
-ailognginx(null, './access.log', pattern);
+function main(argv) {
+    let key, input;
+    
+    key = argv['key'] || argv['k'];
+    input = argv['in'];
+
+    ailognginx(key, input, pattern);
+}
+
+main(argv);

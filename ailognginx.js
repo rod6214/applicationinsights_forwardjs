c6d3bfa45dc63c6ee9101041ccd1a621f8451264
Nelson Amador <rod6214@gmail.com>
2021-05-25 01:33:27 -0300

    Implemented track request

--- ailognginx.js
+++ ailognginx.js
@@ -3,8 +3,10 @@ const appInsights = require('./appInsights');
 
 const ailognginx = async (key, inputFile, format, interval = 300) => {
     watcher(inputFile, (input) => {
+        console.log(input);
         const data = appInsights.convertStringToRequest(format, input, './format.json');
-        console.log(data);
+        const ai = appInsights.create(key);
+        ai.trackTrace(data);
     }, interval);
 }
 
